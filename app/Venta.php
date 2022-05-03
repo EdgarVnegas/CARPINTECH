1fb4df9f6beb9d8d2413215dd24d081c1e724be3
EdgarVnegas <57099899+EdgarVnegas@users.noreply.github.com>
2022-05-02 23:49:02 -0500

    Add files via upload

---
+++ app/Venta.php
@@ -0,0 +1,22 @@
+<?php
+/*
+	CARPINTECH 2691 Equipo 3
+*/ ?>
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Venta extends Model
+{
+    public function productos()
+    {
+        return $this->hasMany("App\ProductoVendido", "id_venta");
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo("App\Cliente", "id_cliente");
+    }
+}
